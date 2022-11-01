fcde94a449dc83df70e4a427a4647b74f53a5b8b
Ahmed Elserafy <ahmed.samer43@yahoo.com>
2022-11-01 02:40:08 +0200

    Add isArabicWord function

--- Sources/Tabby/SwiftUI/Ext/String.swift
+++ Sources/Tabby/SwiftUI/Ext/String.swift
@@ -10,7 +10,14 @@ import SwiftUI
 let TabbySdkBundle = Bundle.module
 
 public extension String {
+  
   var localized: String {
     NSLocalizedString(self, bundle: TabbySdkBundle, comment: "")
   }
+  
+  func isArabicWord()-> Bool{
+        let predicate = NSPredicate(format: "SELF MATCHES %@", "(?s).*\\p{Arabic}.*")
+                return predicate.evaluate(with: self)
+      
+    }
 }
